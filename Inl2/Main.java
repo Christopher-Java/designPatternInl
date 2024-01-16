0dd264ebfc275795a07027afe8b66e96d50d1855
Christopher-Java <chrlom0905@skola.goteborg.se>
2024-01-16 15:35:28 +0100

    Made changes to main for enum

--- Inl2/Main.java
+++ Inl2/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println(pubStatic.toString());
 
         System.out.println("ENUM");
-        System.out.println(EnumSingleton.INSTANCE.toString());
+        System.out.println(EnumSingleton.INSTANCE);
 
     }
 }
